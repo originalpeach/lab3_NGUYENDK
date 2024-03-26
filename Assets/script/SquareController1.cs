47ad6e6d5315964db67356c76697e64ed6a29c5b
originalpeach <NguyenDKPD10037@gmail.com>
2024-03-26 13:08:59 +0700

    ASSIGMENT GD1

--- Assets/script/SquareController1.cs
+++ Assets/script/SquareController1.cs
@@ -9,9 +9,17 @@ public class SquareController : MonoBehaviour
     // Start is called before the first frame update
     public float timeRemaining = 60;
     public Text countdownText;
+    public float moveSpeed = 5f;
+
+    // Start is called before the first frame update
+    public GameObject bulletPrefab;
+    public float bulletSpeed = 10f;
+
+    private Vector2 shootDirection;
     void Start()
     {
-        StartCoroutine(Countdown()); 
+        StartCoroutine(Countdown());
+
     }
     IEnumerator Countdown()
     {
@@ -21,35 +29,100 @@ public class SquareController : MonoBehaviour
             timeRemaining--;
             countdownText.text = "Time: " + timeRemaining.ToString();
         }
-        countdownText.text = "time's up!";
+        countdownText.text = "Time's up!";
     }
-
     // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 movement = new Vector3 (horizontal, vertical, 0f).normalized;
-        transform.Translate(movement * 5f * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            shootDirection = Vector2.left;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            shootDirection = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            shootDirection = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            shootDirection = Vector2.down;
+        }
+
+        // B?n ??n khi ng??i ch?i nh?n Space
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
+        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, moveDirection, out hit, 1.0f))
+        {
+            Renderer renderer = hit.collider.GetComponent<Renderer>();
+
+            if (renderer != null && renderer.material.color == Color.black)
+            {
+                // N?u nhân v?t ch?m vào vùng màu ?en, ng?n nhân v?t di chuy?n
+                transform.Translate(-moveDirection * moveSpeed * Time.deltaTime);
+            }
+        }
+
+
     }
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
-    private void OnCollisionEnter2D(Collision2D col)
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (col.gameObject.name.Equals("circle"))
+        if (collision.gameObject.tag.Equals("Circle"))
         {
-            Debug.Log("xxxx");
-            Vector2 firstPosition = new Vector2(6, 6);
-            transform.position = (Vector3)firstPosition;
+
+            Vector2 fistPosition = new Vector2(-8, 1);
+            transform.position = fistPosition;
+
+        }
+        if (collision.gameObject.name.Equals("Box"))
+        {
+
+            LoadNextScene();
+
+        }
+        if (collision.gameObject.tag.Equals("Pinwheel"))
+        {
+
+            Vector2 fistPosition = new Vector2(-9, 1);
+            transform.position = fistPosition;
+
         }
 
-       if (col.gameObject.name.Equals("box"))
-       {
-            Debug.Log("Win");
-            LoadNextScene();
-       }
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("MapEdge")) // Ki?m tra xem collider khác có ph?i là vi?n b?n ?? không
+        {
+            Debug.Log("xxxxxx");
+            // D?ng di chuy?n c?a GameObject khi va ch?m vào vi?n b?n ??
+            Vector2 fistPosition = new Vector2(-9, 1);
+            transform.position = fistPosition;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject newBullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+        Rigidbody2D bulletRb = newBullet.GetComponent<Rigidbody2D>();
+        if (bulletRb != null)
+        {
+
+            bulletRb.velocity = shootDirection * bulletSpeed;  // B?n theo h??ng "up" c?a GameObject
+        }
     }
 }
